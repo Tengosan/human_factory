604c91dc2b563192c3c9e204ae1563600921fd20
Tengosan <HM.Lost.inc@yandex.ru@domain.com>
2020-09-21 05:05:13 +0700

    Updates

---
+++ precache-manifest.123b2224db6915d1ef842cbb0ffa1984.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc63c32c50c49266a393345452de418e",
+    "url": "/human_factory/index.html"
+  },
+  {
+    "revision": "91e2a11573a1cf121b30",
+    "url": "/human_factory/static/css/main.b86f479b.chunk.css"
+  },
+  {
+    "revision": "94e17c18c3806ac9133b",
+    "url": "/human_factory/static/js/2.93c671e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/human_factory/static/js/2.93c671e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e2a11573a1cf121b30",
+    "url": "/human_factory/static/js/main.0c136e8c.chunk.js"
+  },
+  {
+    "revision": "48f316b4e00b9e3c018f",
+    "url": "/human_factory/static/js/runtime-main.5985c502.js"
+  },
+  {
+    "revision": "ec5a97d2f3b4592eb9afdde21cea8a98",
+    "url": "/human_factory/static/media/arm.ec5a97d2.png"
+  },
+  {
+    "revision": "1dc3e23be15361dc506dbfe63dd3fa81",
+    "url": "/human_factory/static/media/cauldron.1dc3e23b.png"
+  },
+  {
+    "revision": "2231bd489838e1e2319c3a493bc99443",
+    "url": "/human_factory/static/media/coin.2231bd48.png"
+  },
+  {
+    "revision": "04c9c75c1774b3d23c690e456c79ec41",
+    "url": "/human_factory/static/media/cucumber.04c9c75c.png"
+  },
+  {
+    "revision": "7c49c70bd64c56ff47d501031217b421",
+    "url": "/human_factory/static/media/leg.7c49c70b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/human_factory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96e0b25b5bca97142c830c3468f35d71",
+    "url": "/human_factory/static/media/man.96e0b25b.png"
+  },
+  {
+    "revision": "3e6df71c86d17bc81a49675046433af0",
+    "url": "/human_factory/static/media/man_arms.3e6df71c.png"
+  },
+  {
+    "revision": "a787313f86bacef45e425e329e22a530",
+    "url": "/human_factory/static/media/man_cucumber.a787313f.png"
+  },
+  {
+    "revision": "7a253a2095c72be79acda47f376c57dd",
+    "url": "/human_factory/static/media/man_legs.7a253a20.png"
+  },
+  {
+    "revision": "e9196300f03581e352d4402919002d13",
+    "url": "/human_factory/static/media/message_cloud.e9196300.png"
+  },
+  {
+    "revision": "baa408f40db58392cb211f8ff6f1b3e2",
+    "url": "/human_factory/static/media/woman.baa408f4.png"
+  },
+  {
+    "revision": "8118352303e17759907affa53bead1eb",
+    "url": "/human_factory/static/media/woman_arms.81183523.png"
+  },
+  {
+    "revision": "1ab9f9697a5a0c374ed0bc5be820754e",
+    "url": "/human_factory/static/media/woman_cucumber.1ab9f969.png"
+  },
+  {
+    "revision": "dc3f9ac4d4f5c76608017595acb208e8",
+    "url": "/human_factory/static/media/woman_legs.dc3f9ac4.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/human_factory/precache-manifest.123b2224db6915d1ef842cbb0ffa1984.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/human_factory/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
